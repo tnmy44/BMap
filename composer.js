4c75b9f4cb167d076c3a4c5b5c506602f0d32546
Asutosh Palai <asupalai@gmail.com>
2015-02-06 00:20:19 +0530

    Created composer.js

---
+++ composer.js
@@ -0,0 +1,16 @@
+{
+  "name" : "BMap",
+  "description": "Our Maunderer's Map",
+  "keywords": ["mvc", "map", "location sharing", "nearby places"],
+  "require": {
+    "torophp/torophp": "dev-master"
+  },
+  "require-dev": {
+  },
+  "autoload": {
+	  "psr-4": {
+	  //"Controllers\\": "app/controllers/",
+	  //"Models\\": "app/models/"
+	  }
+  }
+}
