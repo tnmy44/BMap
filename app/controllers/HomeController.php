4c6aa0cf5ecb4a3d9e673d4f77860e8e28c86eea
Asutosh Palai <asupalai@gmail.com>
2015-02-06 00:37:57 +0530

    Added index.php  and created controller for root address

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php 
+/*
+
+Controller for / requets
+
+*/
+
+namespace Controllers;
+
+class HomeController {
+    
+    protected $twig;
+    
+    public function __construct() 
+    {
+        
+    }
+    
+    public function get()
+    {
+        
+    }
