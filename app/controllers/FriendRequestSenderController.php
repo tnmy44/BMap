31c124a802344b51f445c81f85b5f20430745284
Punit <punitdhoot1@gmail.com>
2015-02-07 01:41:16 +0530

    Added notification controllers and models

--- app/controllers/FriendRequestSenderController.php
+++ app/controllers/FriendRequestSenderController.php
@@ -12,14 +12,7 @@ class FriendRequstSenderController
 	}
 	public static function post()
 	{
-		$user1 = $_POST['user1'];
-		$user2 = $_POST['user2'];
-		if($user1>$user2)
-		{
-			$temp = $user1;
-			$user1 = $user2;
-			$user2 = $temp;
-		}
+		
 		$updateRequest = FriendRequstHandler::update($user1 , $user2); 	
 		if($updateRequest)
 		{
---
+++ app/controllers/NotificationController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Controllers;
+use Models\Notifications;
+
+class NotificationController
+{
+	protected $twig;
+
+	public static function __construct()
+	{
+		$loader = new \Twig_Loader_Filesystem(__DIR__ . "/../views");
+		$this->twig = new \Twig_Environment($loader);
+	}
+
+	public static function post()
+	{
+		//session_start();
+		$user = $_SESSION['userid'];
+
+		$notifications = Notifications::getNotifications($user);
+		//some confusion here (to echo the json object here or at the front end)@tanmay?
+
+	}
+}
+?>
