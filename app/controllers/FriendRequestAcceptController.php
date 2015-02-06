f07057ca6423e4cb06223f35407b96b8b505e208
Tanmay <tnmy44@gmail.com>
2015-02-07 02:46:21 +0530

    corrected acc FR functionality

--- app/controllers/FriendRequestAcceptController.php
+++ app/controllers/FriendRequestAcceptController.php
@@ -14,16 +14,10 @@ class FriendRequestAcceptController
 		}
 		$user1 = $_SESSION['userid'];
 		$user2 = $_POST['user2'];
+
+		$accCode = FriendRequstAcceptHandler::acceptFR($user1 , $user2); 	
 		
-		$updateRequest = FriendRequstAcceptHandler::acceptFR($user1 , $user2); 	
-		if($updateRequest)
-		{
-			//send "Friend Request accepted!You are now friends"
-		} 
-		else
-		{
-			//send "There was some error in accepting the friend request. Please try again!";
-		}
+		echo('"result" : ' . $accCode . '"');
 
 	}
 }
--- app/models/FriendRequestAcceptHandler.php
+++ app/models/FriendRequestAcceptHandler.php
@@ -24,14 +24,56 @@ class FriendRequestAcceptHandler
 			$me = 2;
 		}
 		
-		$statement = $db->prepare("UPDATE relations SET status = 2 WHERE user1 = :user1 AND user2 = :user2");
-		$statement->bindValue(":user1" , $user1 , \PDO::PARAM_INT);
-		$statement->bindValue(":user2" , $user2 , \PDO::PARAM_INT);
-		$result = $statement->execute();
-		if($result)
-			return true;
-		else
-			return false;
+		$checkQuery = $db->prepare("SELECT * FROM relations WHERE user1 = :user1 AND user2  = :user2");
+		$checkQuery->bindValue(":user1" , $user1 , \PDO::PARAM_INT);
+		$checkQuery->bindValue(":user2" , $user2 , \PDO::PARAM_INT);
+		$checkQuery->execute();
+		
+
+		if($row = $checkQuery->fetch(\PDO::FETCH_ASSOC)){
+
+			$status = $row['status'];
+
+			if($status==0) return 2;
+			if($status==1 || $status==2){
+				if($me==$status) return 3;
+
+				$statement = $db->prepare("UPDATE relations SET status = 3 WHERE user1 = :user1 AND user2 = :user2");
+				$statement->bindValue(":user1" , $user1 , \PDO::PARAM_INT);
+				$statement->bindValue(":user2" , $user2 , \PDO::PARAM_INT);
+				$result = $statement->execute();
+
+				if($result)
+					return 0;
+				else 
+					return 7;
+			}
+			
+			if($status==3){
+				return 2;
+			}
+			
+
+
+		}else{
+
+
+			$statement = $db->prepare("UPDATE relations SET status = 2 WHERE user1 = :user1 AND user2 = :user2");
+			$statement->bindValue(":user1" , $user1 , \PDO::PARAM_INT);
+			$statement->bindValue(":user2" , $user2 , \PDO::PARAM_INT);
+			$result = $statement->execute();
+			if($result)
+				return true;
+			else
+				return false;
+
+		}
+
+
+
+
+
+
 	}
 }
 ?>
