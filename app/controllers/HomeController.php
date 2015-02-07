1de15bab5c3faf207466c3da36c3ef1aab31494d
Tanmay <tnmy44@gmail.com>
2015-02-07 16:51:10 +0530

    changes in send FR

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -22,6 +22,7 @@ class HomeController {
         			document.getElementById('result').innerHTML = 'start';
         			var x = new XMLHttpRequest();
         			x.open("POST",document.getElementById('url').value,true);
+                    x.setRequestHeader("Content-type","application/x-www-form-urlencoded");
         			
         			x.onreadystatechange = function(){
  						if(x.readyState==4 && x.status==200){
@@ -34,8 +35,8 @@ class HomeController {
         	</script>
         </head>
         <body>
-        <input id='url' type='text' placeholder='url'>
-        <input id='body' type='text' placeholder='body'/>
+        <input id='url' type='text' placeholder='url' value='login/'>
+        <input id='body' type='text' placeholder='body' value='username=tnmy44&password=daddy'>
         <button value='POST' onclick='send();'>haha</button>
         <div id='result' style="border:1px solid; width:500px; height:500px;">
         </div>
