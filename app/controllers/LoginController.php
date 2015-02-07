73828ecdcdea056742f77bff003a9540c2b8fe5b
Tanmay <tnmy44@gmail.com>
2015-02-07 12:56:51 +0530

    rectify login controller

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -6,24 +6,28 @@ use Models\User;
 
 class LoginController {
     
-   
+
+    public static function echoresultnexit($id)
+    {
+        echo('"result" : "' . $id . '"');
+        exit();
+    }   
     
     public function post()
     {
 
     	if(!isset($_POST['username']) || !isset($_POST['password']))
     	{
-    		echo "1";
-    		exit();
+    		self::echoresultnexit(1);
     	}
 
     	if(!($_POST['username']) || !($_POST['password']))
     	{
-    		echo "1";
-    		exit();
+    		self::echoresultnexit(1);
     	}
 
     	$loginresult = User::login($_POST['username'],$_POST['password']);
      	
     }
+
 }
