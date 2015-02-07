c1a88b779917caad57a9d6ece7e0732a76bc9e4d
Tanmay <tnmy44@gmail.com>
2015-02-07 13:10:10 +0530

    Seg

---
+++ app/controllers/LoginController.php
@@ -0,0 +1,45 @@
+<<<<<<< HEAD
+<?php 
+
+namespace Controllers;
+
+use Models\User;
+
+class LoginController {
+    
+
+    public static function echoresultnexit($id)
+    {
+        echo('"result" : "' . $id . '"');
+        exit();
+    }   
+    
+    public function post()
+    {
+
+    	if(!isset($_POST['username']) || !isset($_POST['password']))
+    	{
+    		self::echoresultnexit(1);
+    	}
+
+    	if(!($_POST['username']) || !($_POST['password']))
+    	{
+
+    		self::echoresultnexit(1);
+
+    		echo "1";
+    		exit();
+    	}
+
+    	if(!($_POST['username']) || !($_POST['password']))
+    	{
+    		echo "1";
+    		exit();
+
+    	}
+
+    	$loginresult = User::login($_POST['username'],$_POST['password']);
+     	
+    }
+
+}
